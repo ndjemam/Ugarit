db81a36ff0e47bd671296376c0ea18792f83923c
Nidal DJEMAM <ndjemam@snap.fr>
2017-01-12 11:02:55 +0100

    Create project

---
+++ mylibrary/src/main/java/com/example/mylibrary/LibTest.java
@@ -0,0 +1,15 @@
+package com.example.mylibrary;
+
+/**
+ * Created by ndjemam on 12/01/2017.
+ */
+
+public class LibTest {
+    public LibTest(){
+
+    }
+
+    public String getName(){
+        return getClass().getName();
+    }
+}
